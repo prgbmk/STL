18f41d81638675bd53cd677dc3e6beec0f04bb8a
MungiPark <prgbmk@naver.com>
2015-05-11 00:28:42 +0900

    Initial Commit

---
+++ STL_Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  STL_Practice
+//
+//  Created by 박문기 on 2015. 5. 11..
+//  Copyright (c) 2015년 박문기. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
